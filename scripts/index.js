e6aa60106eb75d84bbc271fbe1360291ff7b3a3b
Andrew Josey <JoganLosey@gmail.com>
2019-02-28 12:04:26 -0500

    un messed it up

--- scripts/index.js
+++ scripts/index.js
@@ -6,10 +6,16 @@ const targetElements = document.querySelectorAll("[data-target]");
 // we see an enlarged version of image that is
 // inside that .picture-frame
 
-function respondToClick() {
+function respondToClick(event) {
     console.log('hello.');
     console.log('How do you do?');
     console.log('I am very well, thank you.');
+
+    console.log(event.target.parentElement);
+    // ad the "big" class to the thing that got clicked"
+
+    // event.target.parentElement.classList.add('big');
+
 }
 
 function attachClickHandler(oneElement) {
